f425b3c50776a4ce21a32d59f005ccfe687379d6
brunofelipehp <brunofelipehp8@gmail.com>
2022-01-10 15:14:13 -0300

    NLW React-Native

---
+++ src/components/SigninBox/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import { View } from 'react-native';
+import { COLORS } from '../../theme';
+import { Button } from '../Button';
+
+import { useAuth } from '../../hooks/auth'
+import { styles } from './styles';
+
+export function SigninBox() {
+    const { signIn, isSigningIn } = useAuth();
+    return (
+        <View style={styles.container}>
+
+            <Button
+                title="ENTRAR COM O GITHUB"
+                color={COLORS.BLACK_PRIMARY}
+                backgroundColor={COLORS.YELLOW}
+                icon="github"
+                onPress={signIn}
+                isLoaging={isSigningIn}
+            />
+
+        </View>
+    );
+}
